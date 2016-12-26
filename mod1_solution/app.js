848a10920bb34389afc820655c7ff31fcb7ad639
Srikanth Mettukuru <SNM@MIDASNET.LOCAL>
2016-12-26 01:55:07 -0600

    Code

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -13,11 +13,11 @@ function Func($scope){
   $scope.CheckIfTooMuch = function(){
     var text = $scope.Text;
     var arrStr = text.split(",");
-    if (txt.length == 0){
+    if (text.length == 0){
         $scope.Message = "Please enter data first";
     }
     else{
-      if (arrStr < 4) {
+      if (arrStr.length < 4) {
          $scope.Message = "Enjoy!";
       }
       else{
